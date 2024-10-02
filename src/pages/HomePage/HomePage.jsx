34b5bea4682273da520bf00e77799e77e12e4f64
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-02 14:15:17 +0200

    1

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,14 @@ import { getTrendingMovies } from '../../services/movieService';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { results } = await getTrendingMovies(); // API повертає об'єкт, де масив фільмів знаходиться у полі 'results'
-        setMovies(results);
+        const trendingMovies = await getTrendingMovies();
+        setMovies(trendingMovies.results); // Важливо використовувати results
       } catch (error) {
         console.error('Failed to fetch trending movies:', error);
-        setError('Could not load trending movies. Please try again later.');
       }
     };
 
@@ -22,19 +20,14 @@ const HomePage = () => {
   return (
     <div>
       <h1>Trending Movies</h1>
-      {error && <p>{error}</p>}
       <ul>
         {movies.map(movie => (
           <li key={movie.id}>
             <h2>{movie.title}</h2>
-            {movie.poster_path ? (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-              />
-            ) : (
-              <p>No poster available</p>
-            )}
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
           </li>
         ))}
       </ul>
