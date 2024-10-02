4aa118f4a626149036f30d02f1cb255aa4451dc4
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-02 14:06:06 +0200

    1

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,26 +1,44 @@
 import { searchMovies, getTrendingMovies, getMovieDetails, getMovieCredits, getMovieReviews } from './tmdbApi';
 
-// Пошук фільмів за ключовим словом
-searchMovies('Batman')
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+import axios from 'axios';
 
-// Отримання популярних фільмів
-getTrendingMovies()
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const API_KEY = 'f464d6d375aa30e43301f97dae474950'; // замініть на реальний ключ
 
-// Отримання деталей фільму
-getMovieDetails(12345)
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    Authorization: API_KEY,
+  },
+});
 
-// Отримання акторського складу
-getMovieCredits(12345)
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+export const searchMovies = async (query, page = 1) => {
+  const response = await api.get('search/movie', {
+    params: {
+      query,
+      include_adult: false,
+      language: 'en-US',
+      page,
+    },
+  });
+  return response.data;
+};
 
-// Отримання оглядів
-getMovieReviews(12345)
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+export const getTrendingMovies = async () => {
+  const response = await api.get('trending/movie/day');
+  return response.data;
+};
+
+export const getMovieDetails = async (movieId) => {
+  const response = await api.get(`movie/${movieId}`);
+  return response.data;
+};
+
+export const getMovieCredits = async (movieId) => {
+  const response = await api.get(`movie/${movieId}/credits`);
+  return response.data;
+};
+
+export const getMovieReviews = async (movieId) => {
+  const response = await api.get(`movie/${movieId}/reviews`);
+  return response.data;
+};
